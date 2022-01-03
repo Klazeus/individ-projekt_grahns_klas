36f575684c5d26880d6ff1fb39de2c8490266932
Klazeus <klas.grahns@gmail.com>
2022-01-03 17:56:26 +0100

    Added product cards, components and colors

---
+++ js/eCommerce.js
@@ -0,0 +1,9 @@
+// Variables
+const cartBtn = document.querySelector('.shopping-cart');
+const closeCartBtn = document.querySelector('.close-cart');
+/* const clearCartBtn = document.querySelector(''); */
+const cartDOM = document.querySelector('.cart');
+const cartOverlay = document.querySelector('.cartOverlay');
+const cartItems = document.querySelector('.cart-items');
+/* const cartTotal = document.querySelector('.cart-total'); */
+const cartContent = document.querySelector('.cart-content');
